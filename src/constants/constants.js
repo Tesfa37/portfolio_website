28bd8a3856367a592439e3e8f09c73f8f88e4a13
Tesfatsion Desta <tesfatsion37@gmail.com>
2024-08-08 18:04:16 -0500

    Update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Modern UI/UX GPT-3',
     description: "A modern UI/UX design project inspired by the OpenAI GPT-3 model. The project showcases a clean and responsive design built with React and CSS.",
-    image: 'public/images/1.png',
+    image: '/portfolio_website/public/images/1.png',
     tags: ['React', 'CSS'],
     source: 'https://github.com/Tesfa37/modern_ui_ux_gpt3',
     visit: 'https://tesfa37.github.io/modern_ui_ux_gpt3/',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Restaurant Landing Page',
     description: "A modern UI/UX restaurant landing page. The design focuses on providing a visually appealing and user-friendly experience for restaurant visitors.",
-    image: '/portfolio_website/public/images/3.png',
+    image: '/images/3.png',
     tags: ['React', 'CSS'],
     source: 'https://github.com/Tesfa37/restaurant',
     visit: 'https://tesfa37.github.io/restaurant/',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Bank Modern App',
     description: "A modern UI/UX website for a fictional bank called HooBank. The design focuses on providing a visually appealing and user-friendly experience for banking customers.",
-    image: '/portfolio_website/public/images/2.png',
+    image: '/images/2.png',
     tags: ['React', 'Tailwind CSS', 'Vite'],
     source: 'https://github.com/Tesfa37/bank_modern_app',
     visit: 'https://tesfa37.github.io/bank_modern_app/',
